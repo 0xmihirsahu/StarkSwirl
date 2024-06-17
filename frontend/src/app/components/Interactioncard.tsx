5f80ee244118e236aeb046516b438012747d887d
Paul <mierlo.paul@hotmail.nl>
2024-06-17 12:06:19 +0200

    including tabindex

--- frontend/src/app/components/Interactioncard.tsx
+++ frontend/src/app/components/Interactioncard.tsx
@@ -1,6 +1,10 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 
 export default function Interactioncard() {
+  const [tabIndex, setTabIndex] = useState<number>(1);
+
   // Two children lists, first child list with content cut after and last child with content cut before
   return (
     <div className="tabs flex flex-col ">
@@ -8,6 +12,7 @@ export default function Interactioncard() {
         <ul className="flex items-center justify-between flex-grow-1 flex-shrink-0">
           <li className="m-0 p-0">
             <a
+              onClick={() => setTabIndex(1)}
               className="flex justify-center items-center relative mr-7 m-0 pr-7 border border-red-600 border-solid cursor-pointer hover:bg-pink-700"
               style={{ fontSize: "1.35rem" }}
             >
@@ -16,6 +21,7 @@ export default function Interactioncard() {
           </li>
           <li className="m-0 p-0 ">
             <a
+              onClick={() => setTabIndex(2)}
               className="flex justify-center items-center relative ml-7 m-0 pr-7 border border-red-600 border-solid cursor-pointer hover:bg-pink-700"
               style={{ fontSize: "1.35rem" }}
             >
@@ -28,7 +34,12 @@ export default function Interactioncard() {
         className="flex flex-col bg-dark h-full border border-red-600 border-solid"
         style={{ padding: "1.5rem 1.5rem 2rem" }}
       >
-        <div></div>
+        <div className={tabIndex === 1 ? "active-content" : "content"}>
+          tab1
+        </div>
+        <div className={tabIndex === 2 ? "active-content" : "content"}>
+          tab2
+        </div>
       </section>
     </div>
   );
